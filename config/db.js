3fc6697f8dcae6b8c2cef3bd0e0f93855c85771a
Chun Kit Lam <kitatinternet@gmail.com>
2021-02-27 04:57:50 -0500

    connection to the database business_contect v1
    connect to the local mongo db , add business page to load the contect list from db

---
+++ config/db.js
@@ -0,0 +1,9 @@
+module.exports=
+{
+    "URI": "mongodb://localhost/business_contact"
+    //"URI": "mongodb://localhost/post"
+    //"URI": "mongodb://127.0.0.1:27017/book_store"
+    //"URI": "mongodb://kit:kit009969%40127.0.0.1:27017/book_store?authSource=admin&compressors=zlib&retryWrites=true&w=majority&ssl=true"
+    //"URI": "mongodb://kit:kit009969%40localhost/book_store"
+    //"URI": "mongodb://myTester:xyz123%40127.0.0.1:27017/book_store"
+}
---
+++ models/business.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+// create a model class
+let businessModel = mongoose.Schema({
+    name: String,
+    number: String,
+    email: String
+},
+{
+    collection: "business"
+});
+
+module.exports = mongoose.model('Business', businessModel);
