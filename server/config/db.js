68c5a2825324c2cce4dc96593b41b610b6c91f19
Chun Kit Lam <kitatinternet@gmail.com>
2021-02-28 01:04:25 -0500

    MongoDBServer
    connect to Mongo DB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,5 @@
 module.exports=
 {
-    "URI": "mongodb://localhost/business_contact"
-    //"URI": "mongodb://localhost/post"
-    //"URI": "mongodb://127.0.0.1:27017/book_store"
-    //"URI": "mongodb://kit:kit009969%40127.0.0.1:27017/book_store?authSource=admin&compressors=zlib&retryWrites=true&w=majority&ssl=true"
-    //"URI": "mongodb://kit:kit009969%40localhost/book_store"
-    //"URI": "mongodb://myTester:xyz123%40127.0.0.1:27017/book_store"
+    //"URI": "mongodb://localhost/business_contact"
+    "URI": "mongodb+srv://kit:TA5QNhagEFj7PNPT@mongodbserver.pefy3.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
